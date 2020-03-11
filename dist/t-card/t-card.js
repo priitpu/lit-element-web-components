ba633d2c2723b5eaac2a4d797757193c3d37364a
Priit Puru <purupriit@gmail.com>
2020-03-12 00:06:51 +0200

    stonks

--- dist/t-card/t-card.js
+++ dist/t-card/t-card.js
@@ -8,7 +8,8 @@ import { LitElement, html, customElement, css } from "lit-element";
 let TCard = class TCard extends LitElement {
     static get styles() {
         return css `
-      *, ::slotted(*) {
+      *,
+      ::slotted(*) {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
@@ -16,7 +17,6 @@ let TCard = class TCard extends LitElement {
       :host {
         display: block;
         padding: 18px 16px;
-        margin: 24px 16px;
         background-color: #ffffff;
         font-family: sans-serif;
         border-radius: 2px;
